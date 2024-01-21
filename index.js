9612e8cfd27bb068a8b00c78bcdf74bbfba2e99c
Serhii Myshko <sergeiown@gmail.com>
2024-01-21 11:21:32 +0200

    begin to work with modular structure

--- index.js
+++ index.js
@@ -4,33 +4,7 @@ const { promisify } = require('util');
 const clear = require('clear');
 const https = require('https');
 const { execSync } = require('child_process');
-
-const messages = {
-    en: {
-        installedVersion: 'Installed Node.js version:',
-        latestVersion: 'Latest available Node.js version:',
-        updatePrompt: 'Do you want to update Node.js? (y/n): ',
-        updating:
-            'For the script to work correctly, select the setup suggestion: "Don\'t cose applications".\nUpdating Node.js...',
-        updateError: 'Error updating Node.js:',
-        updateSuccess: 'Node.js updated successfully.',
-        upToDate: 'The Node.js version is up to date or no update is required',
-        languageNotSupported:
-            'Language not supported. Please run the program with "uk" as an argument for Ukrainian language.',
-    },
-    uk: {
-        installedVersion: 'Встановлена версія Node.js:',
-        latestVersion: 'Остання доступна версія Node.js:',
-        updatePrompt: 'Бажаєте оновити Node.js? (y/n): ',
-        updating:
-            'Для коректної роботи скрипта виберіть пропозицію setup: "Do not cose applications".\nОновлення Node.js...',
-        updateError: 'Помилка під час оновлення Node.js:',
-        updateSuccess: 'Node.js оновлено успішно.',
-        upToDate: 'Версія Node.js актуальна або оновлення не потрібне.',
-        languageNotSupported:
-            'Мова не підтримується. Будь ласка, запустіть програму з змінною середовища LANGUAGE=uk для української мови.',
-    },
-};
+const messages = require('./modules/messages');
 
 const unlinkFileAsync = promisify(fs.unlink);
 
